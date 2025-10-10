4710447431a2dad38340760060b547064ebb8a02
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-10-10 18:36:04 -0300

    ✨feat: Introduce MongoDB integration with configuration, dependencies, and repository setup

--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/Application.java
+++ RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/Application.java
@@ -2,8 +2,10 @@ package com.internvision.RESTfulAPIDevelopment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.internvision.RESTfulAPIDevelopment.user.application.repository")
 public class Application {
 
 	public static void main(String[] args) {
--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/user/application/service/UserService.java
+++ RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/user/application/service/UserService.java
@@ -2,8 +2,14 @@ package com.internvision.RESTfulAPIDevelopment.user.application.service;
 
 import com.internvision.RESTfulAPIDevelopment.user.application.api.dto.CreateUserDTO;
 import com.internvision.RESTfulAPIDevelopment.user.domain.User;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface UserService {
 
     User createUser(CreateUserDTO dto);
+
+    List<User> getAllUsers();
 }
