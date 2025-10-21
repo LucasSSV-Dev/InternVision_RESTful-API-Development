93d1958a80957a05df5fb71f2e0cab3cf29b344e
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-10-21 14:34:39 -0300

    ✨feat: Add `updateUser` method in `User` domain for updating user properties from DTO

--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/user/domain/User.java
+++ RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.internvision.RESTfulAPIDevelopment.user.domain;
 
+import com.internvision.RESTfulAPIDevelopment.user.application.api.dto.CreateOrUpdateUserDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @Document(collection = "users")
 public class User {
 
+
     @Id
     private String id;
 
@@ -24,4 +26,15 @@ public class User {
     private boolean active;
     private UserRoleEnum role;
 
+    public User updateUser(String id, CreateOrUpdateUserDTO dto) {
+        this.id = dto.id();
+        this.name = dto.name();
+        this.birthDate = dto.birthDate();
+        this.email = dto.email();
+        this.password = dto.password();
+        this.active = true;
+        this.role = dto.role();
+
+        return this;
+    }
 }
