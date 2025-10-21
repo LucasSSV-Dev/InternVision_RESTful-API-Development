fed9b6dd640ccb910bb7126bbd22a7159f03d46c
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-10-21 19:01:44 -0300

    ✨feat: Add handler for `ChangeSetPersister.NotFoundException` in global exception handler

--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/GlobalHandler/GlobalExceptionHandler.java
+++ RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/GlobalHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.internvision.RESTfulAPIDevelopment.GlobalHandler;
 
 import com.internvision.RESTfulAPIDevelopment.GlobalHandler.dto.ErrorResponseDTO;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +20,16 @@ public class GlobalExceptionHandler {
         return new ErrorResponseDTO(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "An unexpected error occurred. Please contact our support.",
+                List.of()
+        );
+    }
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handleNotFound() {
+        return new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                "The requested resource was not found.",
                 List.of()
         );
     }
