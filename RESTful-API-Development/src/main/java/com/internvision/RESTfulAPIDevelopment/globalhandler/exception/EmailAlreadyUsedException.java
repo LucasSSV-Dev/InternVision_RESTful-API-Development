670f014c3515d70250a28b0f26deab8cc19f4e6f
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-10-23 14:37:58 -0300

    🔥chore: Remove project `RESTful-API-Development` and all related configuration, files, and dependencies

--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/globalhandler/exception/EmailAlreadyUsedException.java
+++
@@ -1,7 +0,0 @@
-package com.internvision.RESTfulAPIDevelopment.globalhandler.exception;
-
-public class EmailAlreadyUsedException extends RuntimeException {
-    public EmailAlreadyUsedException(String message) {
-        super(message);
-    }
-}
--- RESTful-API-Development/src/main/java/com/internvision/RESTfulAPIDevelopment/user/domain/UserRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.internvision.RESTfulAPIDevelopment.user.domain;
-
-public enum UserRoleEnum {
-    ADMIN,
-    USER,
-}
